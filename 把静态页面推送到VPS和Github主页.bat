12232ac73a0d5986c5616a8749440c42edd71ec1
yunfwe <1441923087@qq.com>
2018-06-04 18:03:59 +0800

    博客更新

--- 把静态页面推送到VPS和Github主页.bat
+++ 把静态页面推送到VPS和Github主页.bat
@@ -1,3 +1,3 @@
-hexo clean && hexo g && gulp && rsync -avzz --delete public/* root@yunfwe.cn:/data/wwwroot/blog  &&  rsync -avzz --delete -e "ssh -p 8050" public yunfwe@hinote.ga:/data/blog/ && hexo d && pause
+hexo clean && hexo g && gulp &&   rsync -avzz --delete -e "ssh -p 8050" public yunfwe@hinote.ga:/data/blog/ && hexo d && pause
 
 
